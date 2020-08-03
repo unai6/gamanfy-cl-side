cfee036bb6ec36fed7f6b8891f92d9bef5a89abf
unai gonzalez <unaigoe91@gmail.com>
2020-08-03 20:49:29 +0200

    updarted selec process fields

--- gamanfy-client-side/build/service-worker.js
+++ gamanfy-client-side/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bc07f522638341b8885a54145cbea13.js"
+  "/precache-manifest.e07a2436d8c7b48b96b743ab1272d274.js"
 );
 
 self.addEventListener('message', (event) => {
